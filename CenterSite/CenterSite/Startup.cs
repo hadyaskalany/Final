bab227877c838886752c49b5d33fecc7bd065890
hadyaskalany <45493306+hadyaskalany@users.noreply.github.com>
2018-12-02 19:11:18 +0200

    First Submit

---
+++ CenterSite/CenterSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CenterSite.Startup))]
+namespace CenterSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
